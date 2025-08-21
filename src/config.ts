0e8565873cbb42f13a5fedec500233743afe839c
yun-love <A0313wwh12@outlook.com>
2025-08-21 22:18:31 +0800

    修复了一些已知问题

--- src/config.ts
+++ src/config.ts
@@ -53,22 +53,22 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "yun-love",
+	bio: "无限进步",
 	links: [
-		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
+		// {
+			// name: "Twitter",
+			// icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+			// // You will need to install the corresponding icon set if it's not already included
+			// // `pnpm add @iconify-json/<icon-set-name>`
+			// url: "https://twitter.com",
+		// },
+		// {
+			// name: "Steam",
+			// icon: "fa6-brands:steam",
+			// url: "https://store.steampowered.com",
+		// },
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
